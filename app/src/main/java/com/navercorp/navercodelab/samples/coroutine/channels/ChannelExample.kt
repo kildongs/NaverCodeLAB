6c09e196cfeeeb24d86ebbfecdfb82c8cbc719c6
kildongs <kildongs.up@navercorp.com>
2019-11-19 20:12:54 +0900

    파일 추가

---
+++ app/src/main/java/com/navercorp/navercodelab/samples/coroutine/channels/ChannelExample.kt
@@ -0,0 +1,42 @@
+package com.navercorp.navercodelab.samples.coroutine.channels
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
+import java.nio.Buffer
+import java.nio.ByteBuffer
+
+class ChannelExample {
+
+    val imageProcessingJob = Job()
+
+    val sourceChannel = Channel<Buffer>()
+
+    val context = CoroutineScope(imageProcessingJob )
+
+    fun createChannels() {
+        runBlocking{
+            async(imageProcessingJob) {
+
+                while (imageProcessingJob.isActive == true) {
+                    val frame = sourceChannel.receive()
+
+                }
+
+            }
+        }
+
+    }
+
+
+
+    fun onFrame(frame : ByteBuffer) {
+        runBlocking(imageProcessingJob){
+            sourceChannel.send(frame);
+        }
+    }
+}
